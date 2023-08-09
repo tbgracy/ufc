862e2591ddd385752c4ab3e6e53bf9c9730c1c8f
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2023-08-09 10:50:39 +0300

    use react-router-dom Link component for faster page change

--- src/components/Navbar/NavLink.tsx
+++ src/components/Navbar/NavLink.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+
 export default function NavLink({ target, label, isCurrent }: { target: string, label: string, isCurrent: boolean }) {
-    return <li className={isCurrent ? "current-page" : ""}><a href={target}>{label}</a></li>;
+    // return <li className={isCurrent ? "current-page" : ""}><a href={target}>{label}</a></li>;
+    return <li className={isCurrent ? "current-page" : ""}> <Link to={target}>{label}</Link></li>;
 }
