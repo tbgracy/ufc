d1d3e03062a6b828ae2cbd70a3c953d4a4078254
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2023-08-28 18:43:40 +0300

    refactor: Move firebase credentials to environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,22 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB5SlfSdGf_aPgEDO-Bx-t2U2g1TiZ5eu4",
-  authDomain: "ultimate-frontend-challenge.firebaseapp.com",
-  projectId: "ultimate-frontend-challenge",
-  storageBucket: "ultimate-frontend-challenge.appspot.com",
-  messagingSenderId: "377113773943",
-  appId: "1:377113773943:web:c170e77591d6c4230d4372",
-  measurementId: "G-6Z9JG1BM7M"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
