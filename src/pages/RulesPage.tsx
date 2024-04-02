9f6d76305018a48139bc61866ca8195cc199812e
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-04-02 17:30:47 +0300

    ui: Style Rule page

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -1,27 +1,26 @@
 export default function RulesPage() {
     return <main>
         <h2>Rules</h2>
-        <section>
+        <section className="rule-card">
             <h3>Challengers' rules</h3>
             <p>
                 Every saturday, a new challenge begins and the result of the preceding week is
                 forged in stone.
-                To participate, follow the following steps (these are more guidlines than rules)
+                To participate, follow the following steps (these are more guidlines than rules) :
             </p>
             <ol>
                 <li>Register</li>
                 <li>Go to homepage to see the current design</li>
                 <li>Create a repository with the following pattern for every entry : <pre>ufc-w&lt;week-number&gt;[-anything-you-want];</pre></li>
-                <li>Example: <pre>ufc-w1-my-first-entry</pre> for the first week</li>
                 <li>Push your entry to the repository</li>
             </ol>
             <p>Don't forget to have fun :D</p>
         </section>
-        <section>
+        <section className="rule-card">
             <h3>How points are calculated</h3>
             <p>point = upvotes + lighthouse performance</p>
         </section>
-        <section>
+        <section className="rule-card">
             <h3>Voters' rules</h3>
             <ol>
                 <li>Login</li>
