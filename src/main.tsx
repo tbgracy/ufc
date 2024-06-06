5171c142226a672d99816342282d5949271408c0
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-06-06 15:33:03 +0300

    chore: Install redux and redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { router } from './app/routes'
 import './styles/style.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ServicesContext, initialValue } from './app/contexts';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,10 +22,12 @@ export const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ServicesContext.Provider value={initialValue}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </ServicesContext.Provider>
+    <Provider store={store}>
+      <ServicesContext.Provider value={initialValue}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ServicesContext.Provider>
+    </Provider>
   </React.StrictMode>
 )
